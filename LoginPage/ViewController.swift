aff44695c9970397b7ee5a726d0d102262f0df0e
Your Name <you@example.com>
2018-03-18 10:59:20 +0800

    Initial commit

---
+++ LoginPage/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by WY NG on 8/3/2018.
+//  Copyright © 2018 lumanman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logo: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+    
+     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
